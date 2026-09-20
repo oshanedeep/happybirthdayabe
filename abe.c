2fa7fb989398be2808c935182489849d551c2cfc
oshanedeep <samshanepilapil@gmail.com>
2026-09-20 23:57:26 +0900

    Add files via upload

---
+++ abe.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char name[50];
+
+    do{
+        printf("Enter your name: ");
+        scanf("%s", name);
+    } while (strcmp(name, "Abe")!= 0);
+
+    printf("\n\nHi, Abe! Happy Happy Blessed birthdayyyy sa hev abe ng bohai naming walo UwU <- yan nalang");
+    printf("\nkasi ayaw lumabas ng emojis dito sa complier ko ng code aywan ku bah!");
+    printf("\nso ayun na nga uh panu ba i2 huhu abe iloveloveloveusm ewan ku bah bakit ka umiyak kanina");
+    printf("\npiru i am in the verge of crying na rin kasi omeyak ka hanobayan !");
+    printf("\ncollege na ambebe na yan UwU HAHSAUHAHAHAH");
+    printf("\ndi na rin u minor UwU");
+    printf("\ni hope things will get better on your side abe ku UwU");
+    printf("\nsana next joyride natin convoy na tayo sasakyan HAHSAUSHAHAHAHA");
+    printf("\nsana rin if want mo magshift, matupad o masunod yun :))");
+    printf("\nhoping and praying that this friendship of ours will never end. mahal na mahal kita abe happy blessed birthday ulit!");
+    printf("xenxia k na beh kung maikli tu pinapatulog n ak realjet huhu goodnight!");
+
+    return 0;
+}
